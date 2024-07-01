3256851660b5b2f33e7acc23acd298343db1652a
RafiWithNZ <rafinuril100@gmail.com>
2024-07-02 00:27:18 +0700

    update

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "https://griyapets-api.vercel.app/";
+export const BASE_URL = "https://griyapets-api.vercel.app/api/v1";
 export const token = localStorage.getItem("token");
 export const role = localStorage.getItem("role");
