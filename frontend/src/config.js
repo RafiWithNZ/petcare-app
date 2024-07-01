5a75e2a83e0450cd86a0c63fac6a1e45473ebe51
RafiWithNZ <rafinuril100@gmail.com>
2024-07-01 20:14:52 +0700

    fix deployment errors

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:5000/api/v1";
+export const BASE_URL = "https://griyapets-api.vercel.app/";
 export const token = localStorage.getItem("token");
 export const role = localStorage.getItem("role");
