07dffb192fc18e13e3eae0d04bf02b7de12d6d18
RafiWithNZ <rafinuril100@gmail.com>
2024-07-02 20:19:26 +0700

    updaet

--- backend/index.js
+++ backend/index.js
@@ -21,22 +21,15 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 const corsOptions = {
-  origin: '*',
+  origin: "https://griyapets.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true,
 };
 
+// Middlewares
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://griyapets.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
 
 app.get("/", (req, res) => {
   res.send("API is working");
