1e77f61ba0d805c434a1c3b57e6c908eb25e3278
RafiWithNZ <rafinuril100@gmail.com>
2024-07-03 20:15:42 +0700

    updates

--- backend/index.js
+++ backend/index.js
@@ -30,6 +30,14 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+  });
+
 
 app.get("/", (req, res) => {
   res.send("API is working");
